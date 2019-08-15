04c85892e695f038a8bec141750788894dab4be6
Fcscanf <fcscanf@gmail.com>
2019-08-15 08:05:21 +0800

    Initial commit

---
+++ Vue01/src/main/web/html/vue01/index.js
@@ -0,0 +1,33 @@
+// 创建Vue实例
+window.onload=function () {
+    new Vue({
+        el:"#root",
+        data:{
+            name:"Jack"
+        }
+    });
+
+    var app2 = new Vue({
+        el: '#app-2',
+        text: '',
+        data: {
+            message: '页面加载于 ' + new Date().toLocaleString(),
+            obj: {name: "Tom"},
+            lists:[{name: "Phone", state: 0},
+                {name: "Computer", state: 1},
+                {name: "Role", state: 0}
+            ]
+        },
+        method:{
+            add:function (name) {
+                if (name) {
+                    this.lists.unshift({name:name, state:'0'});
+                    this.text = '';
+                }
+            },
+            del:function(index) {
+                this.lists.splice(index, 1);
+            }
+        }
+    });
+};
