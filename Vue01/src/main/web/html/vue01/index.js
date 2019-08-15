c2fc2856556be4f241e2e4496606ff1465c89eac
Fcscanf <fcscanf@gmail.com>
2019-08-15 08:49:16 +0800

    [Codes-Fcant-Updates]Updates:使用过滤器filters处理数据显示

--- Vue01/src/main/web/html/vue01/index.js
+++ Vue01/src/main/web/html/vue01/index.js
@@ -13,9 +13,9 @@ window.onload=function () {
         data: {
             message: '页面加载于 ' + new Date().toLocaleString(),
             obj: {name: "Tom"},
-            lists:[{name: "Phone", state: 0},
-                {name: "Computer", state: 1},
-                {name: "Role", state: 0}
+            lists:[{name: "Phone", state: '0'},
+                {name: "Computer", state: '1'},
+                {name: "Role", state: '2'}
             ]
         },
         methods:{
@@ -27,6 +27,20 @@ window.onload=function () {
             },
             del(index) {
                 this.lists.splice(index, 1);
+            }
+        },
+        filters:{
+            stateFilter: function (type) {
+                switch (type) {
+                    case '0':
+                        return '未采购';
+                    case '1':
+                        return '采购中';
+                    case '2':
+                        return '已采购';
+                    default:
+                        return '';
+                }
             }
         }
     });
